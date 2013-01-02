a9c27dea74726ca90e433f8a47d5d1a61e76583e
Jean-Fancois Garreau - Binomed <jean.francois.garreau@gmail.com>
2013-01-02 10:08:05 +0100

    initial commit

---
+++ greennfc-samples/src/com/greennfc/tools/samples/MainActivity.java
@@ -0,0 +1,15 @@
+package com.greennfc.tools.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+	}
+
+}
