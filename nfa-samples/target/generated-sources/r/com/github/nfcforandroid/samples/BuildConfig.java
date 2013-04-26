5db22ab371df0ef094d8a9318554e9764c166cce
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2013-04-26 12:18:02 +0200

    update javadoc + add download page

---
+++ nfa-samples/target/generated-sources/r/com/github/nfcforandroid/samples/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.github.nfcforandroid.samples;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
---
+++ nfa-samples/target/generated-sources/r/com/greennfc/tools/apklib/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.greennfc.tools.apklib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
